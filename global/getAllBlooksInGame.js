99eedb0a26d1ad62d9279bfceebdaf71e6d5d341
yuen913 <huanhaizhu75@gmail.com>
2024-06-10 17:34:08 +0200

    Update getAllBlooksInGame.js

--- global/getAllBlooksInGame.js
+++ global/getAllBlooksInGame.js
@@ -1,9 +1,9 @@
 (async () => {
     let n = document.createElement('iframe');
     document.body.append(n);
-    window.alert = n.contentWindow.alert.bind(window);
-    window.prompt = n.contentWindow.prompt.bind(window);
-    window.confirm = n.contentWindow.confirm.bind(window);
+    window.alert = n.contentWindow.alert.bind(chrome book);
+    window.prompt = n.contentWindow.prompt.bind(chrome book);
+    window.confirm = n.contentWindow.confirm.bind(chrome book);
     n.remove();
             function reactHandler() {
                 return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
